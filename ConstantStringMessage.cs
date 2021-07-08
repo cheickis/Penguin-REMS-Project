0a47c0c125392f0d9992f360d943aba5df933131
cheickis <cheick_ismo@yahoo.fr>
2021-07-08 14:32:11 -0300

    fixed pulling one telegramm error Fixed communication with leishen Lidar issue Add Constant Class to handle constant message Add start functionnality Refactor  the ThreeDLidar Class Refactor the TwoDLidar Class

---
+++ ConstantStringMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penguin__REMS_Project
+{
+    class ConstantStringMessage
+    {
+
+        public const String ONE_TELEGRAMM = "Pull a telegramm";
+    }
+}
